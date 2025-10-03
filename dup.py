a80cfb3f4c3c66f91bc4286ca65b2ec199ab801d
jinyus <starboymc4@gmail.com>
2025-10-03 18:02:18 -0500

    refactor: improve code formatting and structure; add pyproject.toml and uv.lock files

--- dup.py
+++ dup.py
@@ -15,13 +15,28 @@ def get_sha256(filepath: Path) -> str:
     return hsh.hexdigest()
 
 
-@click.command(help='Find duplicate files in a directory')
-@click.argument('folder', default='.', type=click.Path(exists=True, path_type=Path, file_okay=False), required=True)
-@click.option('-d', '--delete', help='Delete newest/oldest', default='old', type=click.Choice(['new', 'old']))
-@click.option('-r', '--dry-run', help='Do not delete files', is_flag=True, default=False)
-@click.option('-y', '--yes', help='skip delete confirmation', is_flag=True, default=False)
+@click.command(help="Find duplicate files in a directory")
+@click.argument(
+    "folder",
+    default=".",
+    type=click.Path(exists=True, path_type=Path, file_okay=False),
+    required=True,
+)
+@click.option(
+    "-d",
+    "--delete",
+    help="Delete newest/oldest",
+    default="old",
+    type=click.Choice(["new", "old"]),
+)
+@click.option(
+    "-r", "--dry-run", help="Do not delete files", is_flag=True, default=False
+)
+@click.option(
+    "-y", "--yes", help="skip delete confirmation", is_flag=True, default=False
+)
 def dup(folder: Path, delete: str, dry_run: bool, yes: bool):
-    files = folder.glob('**/*')
+    files = folder.glob("**/*")
     files = [f for f in files if f.is_file()]
 
     hashes: set[str] = set()
@@ -42,25 +57,25 @@ def dup(folder: Path, delete: str, dry_run: bool, yes: bool):
         sort_by_date = sorted(fs, key=lambda f: f.stat().st_mtime)
 
         to_delete = sort_by_date[1:]
-        if delete == 'old':
+        if delete == "old":
             to_delete = sort_by_date[:-1]
 
-        print('\nDuplicates:')
+        print("\nDuplicates:")
         for f in fs:
-            print(f, '(', datetime.fromtimestamp(f.stat().st_mtime), ')')
-        print('')
+            print(f, "(", datetime.fromtimestamp(f.stat().st_mtime), ")")
+        print("")
 
         print(f'To delete ({"newest" if delete == "new" else "oldest"}):')
         for f in to_delete:
             print(f)
-        print('')
+        print("")
 
         for f in to_delete:
-            print(f'rm {f}')
+            print(f"rm {f}")
             if not dry_run:
-                if yes or click.confirm(f'Delete {f}?', default=True):
+                if yes or click.confirm(f"Delete {f}?", default=True):
                     f.unlink()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     dup()
